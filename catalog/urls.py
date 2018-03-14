fb77d6659784aef5f025d49f1e1d4a6b92e4d806
CHARL <charlesdev48@gmail.com>
2018-03-14 16:25:29 +0300

    Adding templates homepage

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,5 +3,8 @@ from . import  views
 
 
 urlpatterns = [
+    
+    path('',views.index, name='index'),
+    
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Book,Author,BookInstance,Genre
 
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    #Generate counts for home page of site
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()
+    num_genres=Genre.objects.count()  # The 'all()' is implied by default.
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'catalog/index.html',
+        context={'num_books':num_books,
+                 'num_genres':num_genres,
+                 'num_instances':num_instances,
+                 'num_instances_available':num_instances_available,
+                 'num_authors':num_authors},
+    )
+    
+    
+    
